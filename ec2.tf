c92b8866dc1525fb5f5ba74e7539f74fecc21f8f
Selvi1410 <88480364+Selvi1410@users.noreply.github.com>
2022-09-24 15:06:13 +0800

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,16 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = "ap-southeast-1"
-}
-
 resource "aws_instance""myec2" {
   ami = "var.ami_id"
   instance_type = "var.instance_type"
